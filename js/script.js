d9e58e7e50fb08558d8473d21534151236333eab
CiroMarra <tensha93@gmail.com>
2024-03-20 17:30:25 +0100

    Feat: add Script for  our team program

--- js/script.js
+++ js/script.js
@@ -12,3 +12,70 @@
 // Organizzare i singoli membri in card/schede
 
 
+const ourTeam = [
+    { 
+        name: 'Wayne',
+        lastname: 'Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+
+    },
+
+    { 
+        name: 'Angela',
+        lastname: 'Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+
+    },
+
+    { 
+        name: 'Wakter',
+        lastname: 'Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+
+    },
+
+    { 
+        name: 'Angela',
+        lastname: 'Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+
+    },
+
+    { 
+        name: 'Scott',
+        lastname: 'Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+
+    },
+
+    { 
+        name: 'Barbara',
+        lastname: 'Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+
+    },
+]
+
+const mainContainer = document.querySelector('#list');
+for(let i = 0; i < ourTeam.length; i++) {
+    const thisMember = ourTeam[i];
+    console.log(`${thisMember.name} ${thisMember.lastname} ${thisMember.role} ${thisMember.image}`);
+    
+    const newLi = `
+     <li>
+        <h2> ${thisMember.name} ${thisMember.lastname} </h2>
+        <span class="ms-txt"> Ruolo ${thisMember.role} </span>
+        <img>${thisMember.image}</img>
+     </li>   
+    `
+    mainContainer.innerHTML += newLi;
+}
+
+
+
